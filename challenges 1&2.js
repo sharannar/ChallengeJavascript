9a8b1e78e4880b834be712ea59a8a848b37f2174
Sharanna Rushing <83149036+sharannar@users.noreply.github.com>
2021-07-03 20:22:38 -0400

    Add files via upload

---
+++ challenges 1&2.js
@@ -0,0 +1,20 @@
+let cup=2
+if (cup < 3){
+    console.log ("Can I have some more coffee?");
+}else{
+    console.log ("I think I'm okay for now!");
+}
+
+console.log ("Does your car need an oil change?")
+let oil= 5000
+if(oil<5000){
+    console.log ("Yes, I need an oil change!");
+}else{
+    console.log ("I do not need an oil change!");
+}
+let car=4000
+if (car>5000){
+    console.log("Yes I need an oil change!");
+}else{
+console.log ("I do not need an oil change!");
+}
